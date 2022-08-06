756b044462eb31cac65db107a84da55f5d3360b5
Ariady Putra <ariady.putra@gmail.com>
2022-08-06 20:20:15 +0700

    Merged upstream

--- src/Game/Data/Asset.hs
+++ src/Game/Data/Asset.hs
@@ -7,12 +7,11 @@ import Graphics.Gloss
 
 data Assets
     = Sprites -- Use Assets instead of Sprites?
-    { _aPlayer :: [Picture]
-    , _aKey    ::  Picture  -- collect keys instead of food
-    , _aDoor   :: [Picture] -- locked | unlocked by collecting all keys
-    , _aBaseTiles  :: [Picture] 
-    , _aCoin  :: Picture -- If required. Spinning coin? 
+    { _aPlayer    :: [Picture]
+    , _aKey       ::  Picture  -- collect keys instead of food
+    , _aDoor      :: [Picture] -- locked | unlocked by collecting all keys
+    , _aBaseTiles :: [Picture] 
+    , _aCoin      ::  Picture -- If required. Spinning coin? 
 --  , we can expand as many as we need later
     }
 makeLenses ''Assets
-
